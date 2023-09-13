a40bfbb715e24a078894b74b7a7446b4331a32db
yuchang.chen <yuchang.chen@hnsquare.com.tw>
2023-09-13 16:16:01 +0800

    fate: 調整 AcaJailController 1.標記跨領域標籤"@CrossOrigin"

--- hnsquare-cms/src/main/java/com/hn2/cms/controller/AcaJailController.java
+++ hnsquare-cms/src/main/java/com/hn2/cms/controller/AcaJailController.java
@@ -9,16 +9,14 @@ import com.hn2.core.dto.DataDto;
 import com.hn2.core.payload.GeneralPayload;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequestMapping("/aca/jail")
+@CrossOrigin
 public class AcaJailController {
 
     @Autowired
