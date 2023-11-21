eb9ffd1aaa8fdc2c32a00ebf2c85b163571d603e
yuchang.chen <yuchang.chen@hnsquare.com.tw>
2023-11-21 14:57:00 +0800

    feat :新增 GlobalCorsConfig

---
+++ hnsquare-config/src/main/java/com/hn2/config/GlobalCorsConfig.java
@@ -0,0 +1,46 @@
+package com.hn2.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+/** 全域請求設置 */
+@Configuration
+public class GlobalCorsConfig {
+
+    @Value("${app.cors.site}")
+    private String corsSite;
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        //允許跨網域請求的來源
+        config.setAllowedOrigins(Arrays.asList(corsSite.split(",")));
+
+        //允許跨域攜帶cookie資訊，預設跨網域請求是不攜帶cookie資訊的。
+        config.setAllowCredentials(true);
+
+        //允許使用那些請求方式
+        config.addAllowedMethod("/*");
+
+        //允許哪些Header
+        config.addAllowedHeader("/*");
+
+        //可獲取哪些Header（因為跨網域預設不能取得全部Header資訊）
+        config.addExposedHeader("/*");
+
+        //映射路徑
+        UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
+        configSource.registerCorsConfiguration("/**", config);
+
+        //return一個的CorsFilter.
+        return new CorsFilter(configSource);
+    }
+
+}
