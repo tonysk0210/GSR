fc1c0b7337f30beaa30ef491ae455c57b5273756
yuchang.chen <yuchang.chen@hnsquare.com.tw>
2023-11-21 11:32:01 +0800

    fix: 修正 AcaJail 查詢 API 發文日期欄位相關問題。

--- hnsquare-report/src/main/java/com/hn2/report/payload/Reprot01Payload.java
+++ hnsquare-report/src/main/java/com/hn2/report/payload/Reprot01Payload.java
@@ -4,7 +4,6 @@ import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import java.time.LocalDate;
 import java.util.List;
 
 @Data
@@ -12,9 +11,6 @@ public class Reprot01Payload {
     /** 項目編號列表 */
     @NotEmpty(message = "項目編號列表不可為空")
     private List<String> itemIdList;
-    /** 簽收日期 */
-    @NotNull(message = "簽收日期為必填欄位")
-    private LocalDate singDate;
     /** 簽收人員 */
     @NotNull(message = "簽收人員為必填欄位")
     private String singUser;
