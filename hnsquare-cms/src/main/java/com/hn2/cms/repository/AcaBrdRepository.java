a56c1b2242709a9bebe7dec0b15b3712c5f79aad
eva.su <eva.su@hnsquare.com.tw>
2024-07-02 19:53:25 +0800

    fix 更新

--- hnsquare-cms/src/main/java/com/hn2/cms/repository/AcaBrdRepository.java
+++ hnsquare-cms/src/main/java/com/hn2/cms/repository/AcaBrdRepository.java
@@ -11,7 +11,7 @@ public interface AcaBrdRepository extends JpaRepository<AcaBrdEntity, String> {
 
     @Query(value = "select RIGHT(REPLICATE('0', 5) + CAST( isnull(SUBSTRING(max(id),8,5),0)        + 1 AS VARCHAR(5)), 5) as id     from ACABrd where id like :findKey", nativeQuery = true)
     String genNewId(String findKey);
-    @Query(value = "select RIGHT(REPLICATE('0', 4 + CAST( isnull(SUBSTRING(max(ACACardNo),4,4),0) + 1 AS VARCHAR(4)), 4)  as aca_id from ACABrd where ACACardNo like :findKey", nativeQuery = true)
+    @Query(value = "select RIGHT(REPLICATE('0', 4) + CAST( isnull(SUBSTRING(max(ACACardNo),4,4),0) + 1 AS VARCHAR(4)), 4)  as aca_id from ACABrd where ACACardNo like :findKey", nativeQuery = true)
     String genNewAcaCardNo(String findKey);
 
 }
